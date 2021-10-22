29c5bc0ee4953047226cbd6b72c0d907f37bfac4
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-10-22 10:18:21 +0530

    Update Find_transition_point.cpp

--- Find_transition_point.cpp
+++ Find_transition_point.cpp
@@ -15,6 +15,9 @@ int main()
 
 // this fn find the transition point
 // we use binary search here
+// we need to find 1st position of 1 and return that index
+// [1,1,1]     -->  in this ans is 0
+// [0,0,0,0,1] -->  in this ans is 4
 int transitionPoint(int a[], int n) {
     
     int low = 0;
@@ -24,13 +27,18 @@ int transitionPoint(int a[], int n) {
         
         int mid = (low + high) / 2;           // initialize mid
         
-        if (a[mid] == 1 && a[mid - 1] == 0){  // it will be a tr point if mid has 1 and its previous element has 0
-            return mid;
+        if (a[mid] == 0) {          // if the mid element is zero increase low as we need to find 1 and array is sorted so it will be in right side
+            low = mid + 1;          
         }
-        else if (a[mid] == 0){  // if mid had zero discard left half so inc low          
-            low = mid + 1;
-        }
-        else{                   // discard right half dec high
+        else if (a[mid] == 1) {
+            
+            // if mid index has 1
+            // 1st see if mid is 0 means 1 found in index 0 then return the mid also if 1 found elsewhere but it is preceeded by 0 then also return mid
+            if (mid == 0 || (mid != 0 and a[mid - 1] == 0)){
+                return mid;
+            }
+            
+            // if none of two condition satisfy, dec high as we need 1st value of 1 which will be in left side
             high = mid - 1;
         }
     }
